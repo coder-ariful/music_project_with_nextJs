c5be6395a23eaa6ef0fffa9fda37901d69503411
sohan-me-01 <sohanshak850@gmail.com>
2025-07-08 01:01:18 +0600

    Featured Courses component work is finish and now doing with why you choose us components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import { WhyChooseUs } from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
     <main className=" min-h-screen bg-black/[.96] antialiased bg-grid-white/[0.5]">
       <HeroSection></HeroSection>
+      <FeaturedCourses></FeaturedCourses>
+      <WhyChooseUs></WhyChooseUs>
     </main>
   );
 }
