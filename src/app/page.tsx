d7dbf753af18f4a04d914df39f189b90559ed5e0
sohan-me-01 <sohanshak850@gmail.com>
2025-07-07 22:05:44 +0600

    Home Hero section is done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
+import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
-    <main className=" min-h-screen bg-black/[.96] antialiased bg-grid-white/[0.5]  ">
-      <h1 className="text-3xl font-bold text-center">
-        Hello World form Next Js
-      </h1>
+    <main className=" min-h-screen bg-black/[.96] antialiased bg-grid-white/[0.5]">
+      <HeroSection></HeroSection>
     </main>
   );
 }
